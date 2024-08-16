6f0350750c1afe48798542541895ecb658893b58
Alison <7997735+jose-alison@users.noreply.github.com>
2024-08-16 00:07:08 -0300

    refactor: refactor client api conversor currency"

---
+++ clients/currency_service.py
@@ -0,0 +1,37 @@
+import requests
+
+class CurrencyConversorService:
+    """
+    Serviço de conversão de moedas utilizando a API AwesomeAPI.
+    """
+
+    def __init__(self) -> None:
+        """
+        Inicializa uma nova instância do serviço de conversão de moedas.
+        """
+        pass
+
+    def converter(self, coin_origin: str, coin_target: str):
+        """
+         Converte o valor de uma moeda de origem para uma moeda de destino.
+
+         :param coin_origin: Código da moeda de origem (por exemplo, 'USD' para Dólar Americano).
+         :type coin_origin: str
+
+         :param coin_target: Código da moeda de destino (por exemplo, 'BRL' para Real Brasileiro).
+         :type coin_target: str
+
+         :return:
+            O valor convertido da moeda de origem para a moeda de destino. 
+            Se a moeda não for encontrada, retorna a mensagem de erro da API.
+         :rtype: str
+        """
+
+        response = requests.get(
+            url=f'https://economia.awesomeapi.com.br/json/last/{coin_origin}-{coin_target}'
+        )
+
+        if response.status_code == 404:
+            return response.json().get('message')
+        return response.json().get(f'{coin_origin}{coin_target}').get('bid')
+    
